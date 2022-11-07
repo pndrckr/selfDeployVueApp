ed8d659322378345b837cef25631af186315977a
Tati <pndrckr@users.noreply.github.com>
2022-11-07 12:37:29 -0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,7 +9,7 @@ const branch = 'build'
     await execa("git", ["checkout", "--orphan", branch]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
-    await execa("npm", ["run", "deploy"]);
+    await execa("npm", ["run", "build"]);
     // await execa("yarn", ["build"]);
     // Understand if it's dist or build folder
     // const folderName = fs.existsSync("dist") ? "dist" : "build";
