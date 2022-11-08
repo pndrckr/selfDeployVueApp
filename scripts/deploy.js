a0d8ff2c962b83d2c116e756549877df871c41ee
Tati <pndrckr@users.noreply.github.com>
2022-11-08 16:23:30 -0300

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,7 @@ import fs from "fs";
 const branch = 'build'
     try {
     // await execa("git", ["checkout", "--orphan", "gh-pages"]);
-    await execa("git", ["checkout", "--orphan", branch]);
+    // await execa("git", ["checkout", "--orphan", branch]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
@@ -15,12 +15,13 @@ const branch = 'build'
     // const folderName = fs.existsSync("dist") ? "dist" : "build";
     const folderName = "dist";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", branch]);
-    console.log("Pushing to "+branch+"...");
-    await execa("git", ["push", "origin", `HEAD:${branch}`, "--force"]);
+    // await execa("git", ["--work-tree", folderName, "commit", "-m", branch]);
+    // console.log("Pushing to "+branch+"...");
+    // await execa("git", ["push", "origin", `HEAD:${branch}`, "--force"]);
+    await execa("git", ["push", "origin", "main", "--force"]);
     await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "main"]);
-    await execa("git", ["branch", "-D", branch]);
+    // await execa("git", ["branch", "-D", branch]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
     // eslint-disable-next-line no-console
